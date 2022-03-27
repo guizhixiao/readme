cd941c9c5c202d9cdb0b49a18423dac529c05f8b
guizhixiao <1002142758@qq.com>
2022-03-27 17:45:42 +0800

    ad

--- abc.sh
+++ abc.sh
@@ -3,16 +3,11 @@
 #================================================================
 #   Copyright (C) 2022 think Ltd. All rights reserved.
 #   
-#   文件名称：reboot.sh
+#   文件名称：abc.sh
 #   创 建 者：think
 #   创建日期：2022年03月27日
 #   描    述：
 #
 #================================================================
 
-#这个是一次启动,在添加一下注释
-ps aux | grep java | grep -v grep 
-#开始写第二次
-tail -f /home/abc.log
-# 第三次
-kill -9 `ps aux | grep java | grep -v grep | awk`
+#新增一个shell文件
